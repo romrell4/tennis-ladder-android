e3f569111b5eea3e36ef0f6f9a44a74995ee209d
Eric Romrell <eric_romrell@byu.edu>
2018-12-07 10:54:16 -0700

    Added suppress for support activity

--- app/src/main/java/com/romrell4/tennisladder/TLActivity.kt
+++ app/src/main/java/com/romrell4/tennisladder/TLActivity.kt
@@ -1,9 +1,11 @@
 package com.romrell4.tennisladder
 
+import android.annotation.SuppressLint
 import android.support.annotation.LayoutRes
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.toolbar.view.*
 
+@SuppressLint("Registered")
 open class TLActivity: AppCompatActivity() {
     override fun setTitle(title: CharSequence?) {
         supportActionBar?.title = title
