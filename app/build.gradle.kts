57ce63a8e26215bea951e582afbe1b749317eba5
Eric Romrell <emromrell14@gmail.com>
2025-07-12 11:12:02 -0700

    Version bump 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.romrell4.tennisladder"
         minSdk = 21
         targetSdk = 36
-        versionCode = 33
-        versionName = "3.2"
+        versionCode = 34
+        versionName = "3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
